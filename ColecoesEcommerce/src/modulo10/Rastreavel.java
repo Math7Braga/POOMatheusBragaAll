aad27a23addf71d9a7ef9f10745fe80e30453607
Math7Braga <108959787+Math7Braga@users.noreply.github.com>
2026-09-10 16:30:27 -0300

    Delete ColecoesEcommerce/src/modulo10 directory

--- ColecoesEcommerce/src/modulo10/Rastreavel.java
+++
@@ -1,9 +0,0 @@
-package modulo10;
-
-/**
- * Contrato de serviço: qualquer classe que precise informar status de
- * rastreamento deve realizar (implementar) esta interface.
- */
-public interface Rastreavel {
-    String getStatusRastreio();
-}
